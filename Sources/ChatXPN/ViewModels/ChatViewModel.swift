3f0fcef483acbb9e33a69667c7120eee39c46b99
karenxpn <karenmirakyan@gmail.com>
2024-05-15 12:21:22 +0400

    chat view model

---
+++ Sources/ChatXPN/ViewModels/ChatViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Karen Mirakyan on 15.05.24.
+//
+
+import Foundation
+import NotraAuth
+
+class ChatViewModel: AlertViewModel, ObservableObject {
+    @Published var loading: Bool = false
+    @Published var showAlert: Bool = false
+    @Published var alertMessage: String = ""
+
+    @Published var chats = [ChatModelViewModel]()
+    
+    var manager: ChatServiceProtocol
+    init(manager: ChatServiceProtocol = ChatService.shared) {
+        self.manager = manager
+    }
+    
+    @MainActor func getChats() {
+        loading = true
+        manager.fetchChatList { result in
+            switch result {
+            case .failure(let error):
+                self.makeAlert(with: error, message: &self.alertMessage, alert: &self.showAlert)
+            case .success(let chats):
+                self.chats = chats.map(ChatModelViewModel.init)
+            }
+            
+            self.loading = false
+        }
+    }
+}
