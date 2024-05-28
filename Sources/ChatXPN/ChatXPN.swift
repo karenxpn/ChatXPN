a42c167623d80b98b77b5e1d9ef484ccb2cd36a1
karenxpn <karenmirakyan@gmail.com>
2024-05-28 15:34:06 +0400

    chat room as a navigation stack page

--- Sources/ChatXPN/ChatXPN.swift
+++ Sources/ChatXPN/ChatXPN.swift
@@ -5,8 +5,7 @@ import SwiftUI
 
 public struct ChatXPN: View {
     @EnvironmentObject private var viewRouter: ChatViewRouter
-    @Environment(\.dismiss) var dismiss
-
+    
     let chat: ChatModelViewModel?
     
     public init(chat: ChatModelViewModel? = nil) {
@@ -14,21 +13,10 @@ public struct ChatXPN: View {
     }
     
     public var body: some View {
-        NavigationStack(path: $viewRouter.chatPath) {
-            if let chat {
-                ChatRoom(chat: chat)
-                    .toolbar(content: {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Button {
-                                dismiss()
-                            } label: {
-                                Image("close_popup", bundle: .module)
-                                    .scaleEffect(0.8)
-                                    .padding()
-                            }
-                        }
-                    })
-            } else {
+        if let chat {
+            ChatRoom(chat: chat)
+        } else {
+            NavigationStack(path: $viewRouter.chatPath) {
                 Chat()
                     .navigationDestination(for: ChatPath.self) { page in
                         viewRouter.buildChatView(page: page)
