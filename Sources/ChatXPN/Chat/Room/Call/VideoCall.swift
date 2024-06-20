9cf8fd7de12578a6ab12c30ca4ec6ea26f3301f0
karenxpn <karenmirakyan@gmail.com>
2024-06-20 17:39:32 +0400

    code view changes and onReceive changed to dismiss

--- Sources/ChatXPN/Chat/Room/Call/VideoCall.swift
+++ Sources/ChatXPN/Chat/Room/Call/VideoCall.swift
@@ -13,16 +13,16 @@ struct VideoCall: View {
     @StateObject private var viewModel: CallViewModel
     @Environment(\.dismiss) var dismiss
     private var client: StreamVideoUI
-
+    
     private let apiKey: String
     private let userId: String = Auth.auth().currentUser?.uid ?? ""
     private let callId: String
     private let create: Bool
     private let members: [Member]
     private let endCall: (String) -> ()
-
+    
     @State private var eventSubscriptionTask: Task<Void, Error>?
-
+    
     init(
         token: String,
         callId: String,
@@ -36,22 +36,22 @@ struct VideoCall: View {
         self.apiKey = apiKey
         self.members = users.map { Member(user: User(id: $0.id, name: $0.name)) }
         self.endCall = endCall
-
+        
         let user = User(
             id: userId,
             name: Auth.auth().currentUser?.displayName ?? "User",
             imageURL: URL(string: "https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/")
         )
-
+        
         let customSound = Sounds()
         // Tell the SDK to pick the custom ring tone
         customSound.bundle = Bundle.main
         // Swap the outgoing call sound with the custom one
         customSound.outgoingCallSound = "ringing.mp3"
-
+        
         // Create an instance of the appearance class
         let customAppearance = Appearance(sounds: customSound)
-
+        
         self.client = StreamVideoUI(
             streamVideo: StreamVideo(
                 apiKey: apiKey,
@@ -60,58 +60,39 @@ struct VideoCall: View {
             ),
             appearance: customAppearance
         )
-
+        
         let callViewModel = CallViewModel()
         callViewModel.participantAutoLeavePolicy = LastParticipantAutoLeavePolicy()
         _viewModel = .init(wrappedValue: callViewModel)
-
+        
         print("initialized the video call view")
     }
-
+    
     var body: some View {
         VStack {
             if viewModel.call != nil {
-                CallContainer(
-                    viewFactory: DefaultViewFactory.shared,
-                    viewModel: viewModel
-                )
+                CallContainer(viewFactory: DefaultViewFactory.shared, viewModel: viewModel)
             } else {
-                Text("loading...")
+                Text("loading"~)
             }
         }.task {
-            guard viewModel.call == nil, create else {
-                return
-            }
-
-            viewModel.startCall(
-                callType: .default,
-                callId: callId,
-                members: members,
-                ring: true
-            )
+            guard viewModel.call == nil, create else { return }
+            viewModel.startCall(callType: .default, callId: callId, members: members, ring: true)
         }
-        .onReceive(viewModel.$callingState.map { $0 == .idle }) { if $0 { dismiss() } }
-
+        .onReceive(NotificationCenter.default.publisher(for: .init(CallNotification.callEnded))) { _ in dismiss() }
         .onReceive(viewModel.$callingState.map {
-                        switch $0 {
-                        case .incoming:
-                            return true
-                        default:
-                            return false
-                        }
-            }) { hasIncomingCall in
-            guard hasIncomingCall else {
-                return
+            switch $0 {
+            case .incoming:     return true
+            default:            return false
             }
-            viewModel.acceptCall(
-                callType: .default,
-                callId: callId
-            )
+        }) { hasIncomingCall in
+            guard hasIncomingCall else { return }
+            viewModel.acceptCall(callType: .default, callId: callId)
         }
         .onReceive(viewModel.$call, perform: { newCall in
             eventSubscriptionTask?.cancel()
             eventSubscriptionTask = nil
-
+            
             guard let newCall else { return }
             subscribeToCallEvents(on: newCall)
         })
@@ -121,15 +102,15 @@ struct VideoCall: View {
             Text(viewModel.error?.localizedDescription ?? "")
         })
     }
-
+    
     private func subscribeToCallEvents(on call: Call) {
         eventSubscriptionTask = Task {
             print("subscribe to events")
-
+            
             for await event in call.subscribe(for: CallEndedEvent.self) {
                 print("call ended event", event)
             }
-
+            
             for await event in call.subscribe() {
                 print("event \(event)")
             }
