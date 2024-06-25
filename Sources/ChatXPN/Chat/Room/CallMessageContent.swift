d046b36377f4c3db6bfa3c6974c88f04348caeea
karenxpn <karenmirakyan@gmail.com>
2024-06-25 11:57:41 +0400

    loading call join

--- Sources/ChatXPN/Chat/Room/CallMessageContent.swift
+++ Sources/ChatXPN/Chat/Room/CallMessageContent.swift
@@ -10,6 +10,7 @@ import NotraAuth
 
 struct CallMessageContent: View {
     let message: MessageViewModel
+    @EnvironmentObject var roomVM: RoomViewModel
     
     var body: some View {
         
@@ -24,10 +25,17 @@ struct CallMessageContent: View {
                     VStack {
                         TextHelper(text: message.content, color: !message.received ? .white : .primary)
                         if message.callEnded == false {
-                            TextHelper(text: "joinCall"~, color: .white)
-                                .padding(.horizontal, 10)
+                            HStack(spacing: 8) {
+                                TextHelper(text: "joinCall"~, color: .white)
+                                
+                                if roomVM.loadingCall {
+                                    ProgressView()
+                                        .scaleEffect(0.5)
+                                }
+                            }.padding(.horizontal, 10)
                                 .padding(.vertical, 6)
                                 .background(RoundedRectangle(cornerRadius: 16).fill(.green))
+
                         }
                     }
                 }
