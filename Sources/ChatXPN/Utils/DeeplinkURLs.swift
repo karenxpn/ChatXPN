9c9a293dea880c20a959b809ab41df97e85dd237
karenxpn <karenmirakyan@gmail.com>
2024-07-22 12:41:06 +0400

    Depplink URLs added

---
+++ Sources/ChatXPN/Utils/DeeplinkURLs.swift
@@ -0,0 +1,17 @@
+//
+//  DeeplinkURLs.swift
+//
+//
+//  Created by Karen Mirakyan on 22.07.24.
+//
+
+import Foundation
+enum DeeplinkURLs: String {
+    case home
+    case newDocument
+    case document
+    case chat
+    case account
+    case store
+    case verifyAccount
+}
